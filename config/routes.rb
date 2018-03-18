58ab807fe999a45319ec40b207025deeb6b0825f
Jason Aloi <j@jasonaloi.com>
2018-03-18 14:37:17 -0500

    Components controller w Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'components#index'
+  get 'components/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
